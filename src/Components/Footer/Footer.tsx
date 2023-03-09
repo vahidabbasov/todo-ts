14b589d2a44c7d507a247797fbc39d73c0b66542
vahidabbasov <tu7hl7kxu@code.edu.az>
2023-03-09 21:59:41 +0400

    some changes

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -3,13 +3,10 @@ import styles from './Footer.module.css'
 function Footer() {
   return (
     <footer className={styles.info}>
-      <p>Click to edit a todo</p>
       <p>
-        Created by <a href="https://d12n.me/">Dmitry Sharabin</a>
+        Created by Abbasov
       </p>
-      <p>
-        Part of <a href="http://todomvc.com">TodoMVC</a>
-      </p>
+     
     </footer>
   )
 }
